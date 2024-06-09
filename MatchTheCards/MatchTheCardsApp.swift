467a7308d128ebf39e09f8c5fed916381fbeec2f
Can Kaynar <cannkaynarr@outlook.com>
2024-06-09 16:27:27 +0300

    Initial Commit

---
+++ MatchTheCards/MatchTheCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchTheCardsApp.swift
+//  MatchTheCards
+//
+//  Created by Can on 9.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MatchTheCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
